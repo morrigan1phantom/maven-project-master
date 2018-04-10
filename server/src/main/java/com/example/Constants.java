b74acf71cb60fa8c59a3ea4353fbdb597cd5ff62
morrigan_ph <morrigan1phantom@gmail.com>
2018-04-10 11:15:51 +0300

    Added new constant

--- server/src/main/java/com/example/Constants.java
+++ server/src/main/java/com/example/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
 	public static final String APP_NAME = "webAppExample";
 	
 	public static final String GREET = "Hello";
+	
+	public static final String GREET_NIGHT = "Good night";
 }
